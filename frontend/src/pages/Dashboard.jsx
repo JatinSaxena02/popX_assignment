db656b08ec2f5b48b39bbb5d0073ae8e5cc1b93b
JatinSaxena2002 <jatinsaxena2704@gmail.com>
2025-09-03 22:36:46 +0530

    first commit

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import image from "../assets/image.png";
+import camera from "../assets/camera.png";
+
+function Dashboard() {
+  const { token, logout } = useAuth();
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/");
+      return;
+    }
+    const fetchUser = async () => {
+      const res = await axios.get("http://localhost:5000/me", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      setUser(res.data);
+    };
+    fetchUser();
+  }, [token, navigate]);
+
+  return (
+    <div className="flex justify-center items-center bg-white min-h-screen">
+      <div className="w-[375px] h-[700px] bg-[#F7F8F9] flex flex-col px-5">
+        <div className="bg-white w-[375px]  h-[80px]  -left-5 relative">
+          <h1 className="text-[28px] font-bold text-[#1D2226] mb-8 mt-5 ml-6">
+            Account Settings
+          </h1>
+        </div>
+        <div className="flex-col flex mt-10 gap-4">
+          <div className="flex-row flex">
+            <img src={image} alt="photo" className="w-20 h-20" />
+            <img
+              src={camera}
+              alt="photo"
+              className="w-6 h-6 relative -left-4 top-13"
+            />
+            {user && (
+              <div>
+                <p className="text-[#1D2226] text-[20px] font-bold">
+                  {user.name}
+                </p>
+                <p>{user.email}</p>
+              </div>
+            )}
+          </div>
+          <div>
+            <p className="font-medium text-[16px]">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem
+              ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum
+              dolor sit, amet consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+        <div className="mt-7 outline-1 outline-gray-500 outline-dashed w-[370px] -left-4 relative" />
+        <button
+          className="w-[335px] top-[640px] bg-[#6C25FF] text-white text-sm font-medium py-3 rounded-md mt-4 cursor-pointer absolute"
+          onClick={() => {
+            setUser(null);
+            logout();
+            navigate("/");
+          }}
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+}
+export default Dashboard;
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,157 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+const Register = () => {
+  const { login } = useAuth();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    company: "",
+    password: "",
+    agency: "no",
+  });
+  const navigate = useNavigate();
+  const handleChange = (e) =>
+    setForm({ ...form, [e.target.name]: e.target.value });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // prevent page reload
+    try {
+      const res = await axios.post("http://localhost:5000/register", form);
+      login(res.data.token);
+      // alert("Registered successfully!");
+      navigate("/me");
+    } catch (error) {
+      console.error(error);
+      alert("Something went wrong!");
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center bg-white min-h-screen">
+      <div className="w-[375px] h-[700px] bg-[#F7F8F9] p-6 flex flex-col">
+        <h1 className="text-[28px] font-bold text-[#1D2226] mb-9">
+          Create your <br /> PopX account
+        </h1>
+
+        {/* Content Form */}
+        <form className="flex-1 space-y-8" onSubmit={handleSubmit}>
+          <div className="relative w-full">
+            <label className="absolute top-0 left-2 -translate-y-3/5 bg-[#F7F8F9] pl-2 pr-2 text-sm font-medium text-[#6C25FF] mb-1">
+              Full Name<span className="text-red-500">*</span>
+            </label>
+            <input
+              required
+              onChange={handleChange}
+              value={form.name}
+              name="name"
+              type="text"
+              placeholder="Marry Doe"
+              className="w-full border-[2px] border-[#CBCBCB] rounded-md px-3 py-2 text-sm outline-none"
+            />
+          </div>
+
+          <div className="relative w-full">
+            <label className="absolute top-0 left-2 -translate-y-3/5 bg-[#F7F8F9] pl-2 pr-2 text-sm font-medium text-[#6C25FF] mb-1">
+              Phone number<span className="text-red-500">*</span>
+            </label>
+            <input
+              required
+              onChange={handleChange}
+              value={form.phone}
+              name="phone"
+              type="text"
+              placeholder="1234567890"
+              className="w-full border-[2px] border-[#CBCBCB] rounded-md px-3 py-2 text-sm outline-none"
+            />
+          </div>
+
+          <div className="relative w-full">
+            <label className="absolute top-0 left-2 -translate-y-3/5 bg-[#F7F8F9] pl-2 pr-2 text-sm font-medium text-[#6C25FF] mb-1">
+              Email address<span className="text-red-500">*</span>
+            </label>
+            <input
+              required
+              onChange={handleChange}
+              value={form.email}
+              name="email"
+              type="email"
+              placeholder="marry@example.com"
+              className="w-full border-[2px] border-[#CBCBCB] rounded-md px-3 py-2 text-sm outline-none"
+            />
+          </div>
+
+          <div className="relative w-full">
+            <label className=" absolute top-0 left-2 -translate-y-3/5 bg-[#F7F8F9] pl-2 pr-2 text-sm font-medium text-[#6C25FF] mb-1">
+              Password<span className="text-red-500">*</span>
+            </label>
+            <input
+              required
+              onChange={handleChange}
+              value={form.password}
+              name="password"
+              type="password"
+              placeholder="••••••••"
+              className="w-full border-[2px] border-[#CBCBCB] rounded-md px-3 py-2 text-sm outline-none"
+            />
+          </div>
+
+          <div className="relative w-full">
+            <label className=" absolute top-0 left-2 -translate-y-3/5 bg-[#F7F8F9] pl-2 pr-2 text-sm font-medium text-[#6C25FF] mb-1">
+              Company name
+            </label>
+            <input
+              required
+              onChange={handleChange}
+              value={form.company}
+              name="company"
+              type="text"
+              placeholder="My Company"
+              className="w-full border-[2px] border-[#CBCBCB] rounded-md px-3 py-2 text-sm outline-none"
+            />
+          </div>
+          <div className="-mt-4">
+            <p className="text-sm font-medium text-[#1D2226] mb-2">
+              Are you an Agency?<span className="text-red-500">*</span>
+            </p>
+            <div className="flex space-x-6">
+              <label className="flex items-center space-x-2 text-sm text-gray-700">
+                <input
+                  type="radio"
+                  name="agency"
+                  value="yes"
+                  className="accent-[#6C25FF]"
+                  checked={form.agency === "yes"}
+                  onChange={handleChange}
+                />
+                <span>Yes</span>
+              </label>
+              <label className="flex items-center space-x-2 text-sm text-gray-700">
+                <input
+                  type="radio"
+                  name="agency"
+                  value="no"
+                  className="accent-[#6C25FF]"
+                  checked={form.agency === "no"}
+                  onChange={handleChange}
+                />
+                <span>No</span>
+              </label>
+            </div>
+          </div>
+          {/* Button at bottom */}
+          <button
+            type="submit"
+            className="w-[335px] top-[640px] bg-[#6C25FF] text-white text-sm font-medium py-3 rounded-md mt-4 absolute"
+          >
+            Create Account
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
